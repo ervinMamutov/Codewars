354722f8b7a0a4d041cef63587804268b5721209
ervin.news@gmail.com <ervin.news@gmail.com>
2023-03-20 21:47:33 +0100

    task finished

---
+++ beginner- lost-without-map.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+Description:
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x){
+  const result = x.map(x => x * 2);
+  return result;
+}
