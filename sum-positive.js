4c4c000b84750692dad9ef837e7bacd4d1415018
ervin.news@gmail.com <ervin.news@gmail.com>
2023-03-20 21:46:36 +0100

    task finished

---
+++ sum-positive.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i of arr) {
+    if (i > 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
